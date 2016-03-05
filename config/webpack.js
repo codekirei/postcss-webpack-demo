f783b69987bc8c15373457e81701b46e9cec77f1
Jacob Blakely <jacob@codekirei.com>
2016-03-04 20:27:27 -0800

    init

---
+++ config/webpack.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const autoprefixer = require('autoprefixer')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const cwd = process.cwd()
+
+module.exports = {
+  entry: path.resolve(cwd, 'src', 'app'),
+  output: {
+    path: path.join(cwd, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract([
+          'css',
+          'postcss',
+        ]),
+      },
+    ],
+  },
+  postcss: () => [
+    autoprefixer,
+  ],
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ],
+}
